cfa123954b5f48089a628f1077bbe98d605d353e
Journey <hhh8333@gmail.com>
2021-08-04 14:46:02 +0900

    타이핑 애니메이션 & 애니메이션 스킵

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,37 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using System;
 
 public class DialogueManager : MonoBehaviour
 {
     public DialogueData dialogueData;
-
+    
+    public Text text_dialogue;
     [SerializeField] Text text_name;
-    [SerializeField] Text text_dialogue;
 
-    void Start()
+    TextEffect textEffect;
+    public float delay = 1f;
+    public float interval = 0.2f;
+
+    Coroutine croutine_typing;
+
+    private void Awake()
     {
-        Debug.Log(Application.dataPath);
+        textEffect = GetComponent<TextEffect>();
+    }
 
-        LoadFromJson();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            SaveToJson();
+
+        if (Input.GetKeyDown(KeyCode.L))
+            LoadFromJson();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            SkipTypingEffect();
+    }
+
+    private void SkipTypingEffect()
+    {
+        StopCoroutine(croutine_typing);
+
+        text_dialogue.text = dialogueData.dialogue;
     }
 
     [ContextMenu("Save To Json Data")]
     void SaveToJson()
     {
         string json = JsonUtility.ToJson(dialogueData, true);
-        string path = Path.Combine(Application.dataPath, "dialogueData.json");
-        File.WriteAllText(path, json);
+        string path = Path.Combine(Application.dataPath, "Resources");
+        path = Path.Combine(path, "dialogueData.json"); File.WriteAllText(path, json);
     }
 
     [ContextMenu("Load From Json Data")]
     void LoadFromJson()
     {
-        string path = Path.Combine(Application.dataPath, "dialogueData.json");
+        string path = Path.Combine(Application.dataPath, "Resources");
+        path = Path.Combine(path, "dialogueData.json");
         string json = File.ReadAllText(path);
         dialogueData = JsonUtility.FromJson<DialogueData>(json);
 
         text_name.text = dialogueData.name;
+        text_dialogue.text = "";
+
+        if (Application.isPlaying)
+        {
+            croutine_typing = StartCoroutine(textEffect.OnTypingEffect(dialogueData.dialogue));
+            return;
+        }
+
         text_dialogue.text = dialogueData.dialogue;
     }
 }
---
+++ Assets/Scripts/Skip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skip : MonoBehaviour
+{
+    public void OnSkipEffect()
+    {
+        DialogueManager dialogueManager;
+
+
+    }
+}
